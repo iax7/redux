e391ef53d7f1e2dfdfaf5530df3d4a9cad56a436
Isaias Piña <iax7@users.noreply.github.com>
2021-02-09 23:18:18 -0600

    Add project

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -3,7 +3,7 @@
 //          ^ wraps window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 import { configureStore } from '@reduxjs/toolkit';
 // ^ This replaces both imports from above
-import reducer from './bugs';
+import reducer from './projects';
 
 export default function() {
   return configureStore({ reducer })
